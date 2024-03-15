06d2900683dc5405735c384c5a9933b5b140b8a3
LuckY828CactuS <125570968+LuckY828CactuS@users.noreply.github.com>
2024-03-15 05:54:01 +0300

    Update kliker2.cs

--- kliker2.cs
+++ kliker2.cs
@@ -1,18 +1,3 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-
-public class PlayerScript : MonoBehaviour
-{
-
- void OnTriggerEnter2D(Collider2D other){// столкновение объекта (меч, щит)
-// проверка на соответствие тэга
-  if (other.gameObject.tag != gameObject.tag) { 
-   Time.timeScale = 0;
-   MainScript.score = 0;
-  } else {
-   MainScript.score++;
-   Destroy (other.gameObject);// уничтожение префаба
-  }
- }
-}
+using Unity
